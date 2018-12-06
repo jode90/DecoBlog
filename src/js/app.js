5a4a68b7d927014be3d92212812e4a3c14511572
programer10 <programer10@ucione.local>
2018-12-06 21:27:40 +0100

    Login modal

--- src/js/app.js
+++ src/js/app.js
@@ -1,42 +1,26 @@
-var handleContent = function(){
-	var preloader = $('.preloader');
-	var ajaxLoader = '<img class="ajax-loader" src="/img/loader.gif" />';
-	$('.btn').click(function(e){
+var handleLogin = function(){
+	
+	// get form element
+var loginForm = $('#loginForm');
+
+	// add submit event listener to login form
+	loginForm.on('submit', function(e){
 		e.preventDefault();
-		
-		$.ajax({
-			url: "data/users.json",
-			type: "GET",
-			data: {"id": 1},
-			beforeSend: function(){
-				preloader.empty();
-				preloader.append(ajaxLoader);				
-			},
-			success: function (result, status, xhr){
-				/*ponekad je potrebno parsirati JSON koji dobijete rezultatu u js objektu*/
-				//var users= $.parseJSON(result);
-				console.log(users);
-			},
-			error: function (xhr, status, error){
-			if(error){
-				preloader.empty();
-				preloader.text("An error occured while proccessing your request! Please try again later!");
-				setTimeout(function (){
-				preloader.empty();
-				},5000);
-			}
-			},
-			complete: function (){
-				preloader.empty();
-			}
-		});
+		$(this).parsley({
+			errorsWrapper: '<div class="text-danger"></div>',
+			errorTemplate: '<span></span>'
+		}).validate();
+		if($(this).parsley().isValid()){
+			var data = $(this).serialize();
+			console.log(data);
+		}
 	});
 };
 
 var App = function(){
 	return{
-		init: function(element){
-			handleContent();
+		init: function(){
+			handleLogin();
 		}
 	};
 }();
